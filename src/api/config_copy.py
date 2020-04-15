39e342fafce8432f68e4ac6bbc7acbf42035ae6d
yenlow <yenlow@gmail.com>
2020-04-15 11:37:13 -0700

    remove config.py

--- src/api/config_copy.py
+++ src/api/config_copy.py
@@ -1,22 +1,24 @@
 # Config params
+# Fill in with your credentials and rename file to config.py
+
 import os
 
 #for Google API
-gspread_url = 'https://docs.google.com/spreadsheets/d/1itaohdPiAeniCXNlntNztZ_oRvjh0HsGuJXUJWET008/edit?usp=sharing'
-credentials_google = os.environ.get("GOOGLE_APPLICATION_CREDENTIALS")
+gspread_url = 'http://xxx'
+credentials_google = os.environ.get("GOOGLE_APPLICATION_CREDENTIALS") #assuming you save keys as env variables
 
 #for Github v3 API
-credentials_github = os.environ.get("GITHUB_ACCESS_TOKEN")
+credentials_github = os.environ.get("GITHUB_ACCESS_TOKEN") #assuming you save keys as env variables
 
 def twitter_api():
-    consumer_key = "ejD3rnJzJPrk4pomI6Jj7xP7g"
-    consumer_secret = "F8ylc7Eogk1IX4NMV47xI21fJ7gh8U8Kg5AGfIvmCizwg6YkjC"
-    access_token = "69460295-I8ujKvCbwrluNsmIha7uPypSFm5dEaHm9BLIlOJnn"
-    access_secret = "KgER7Ci98IR9cVej86N9BRqd6hfkPgx3ywHDqn7ndvfeA"
+    consumer_key = "xxx"
+    consumer_secret = "xxx"
+    access_token = "xxx"
+    access_secret = "xxx"
     return consumer_key, consumer_secret, access_token, access_secret
 
 def aws_api():
     region = 'us-west-1'
-    access_key_id = 'AKIATLC3GIPSQ4QVCZ5E'
-    access_secret = '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'
+    access_key_id = 'xxx'
+    access_secret = 'xxx'
     return region, access_key_id, access_secret
